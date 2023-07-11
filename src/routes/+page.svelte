37f38ec9ca632a89f539e2eec0d61e50340a26e4
Razvan Bielz <razvan.bielz@gmail.com>
2023-07-11 15:38:09 +0300

    Added fetches for HomePage Created Custom fetch-refresh function Added Window interface

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import type { PageData } from './$types';
 
-<div style="height: 1000px" />
-<input type="text" />
-<select name="a" id="s">
-	<option value="option">option</option>
-</select>
-<button>button</button>
-
-<style lang="scss">
-	h1 {
-		color: red;
-		font-size: functions.toRem(32);
-		@include breakpoint.up('md') {
-			color: blue;
-		}
-		@include breakpoint.up('lg') {
-			color: green;
-		}
-	}
-</style>
+	export let data: PageData;
+</script>
---
+++ src/routes/+page.ts
@@ -0,0 +1,44 @@
+import type { PageLoad } from './$types';
+import { fetchRefresh } from '$helpers';
+
+export const load: PageLoad = async ({ fetch: _fetch }) => {
+	const fetch = (path: string) => fetchRefresh(_fetch, path);
+
+	const newReleases = fetch('/api/spotify/browse/new-releases?limit=6');
+	const featuredPlaylists = fetch('/api/spotify/browse/featured-playlists?limit=6');
+	const myPlaylists = fetch('/api/spotify/me/playlists?limit=6');
+
+	const catsRes = await fetch('/api/spotify/browse/categories');
+	const catsResJSON: SpotifyApi.MultipleCategoriesResponse | undefined = catsRes.ok
+		? await catsRes.json()
+		: undefined;
+
+	const randomCats = catsResJSON
+		? catsResJSON.categories.items.sort(() => Math.random() - 0.5).slice(0, 3)
+		: [];
+
+	const randomCatPromises = randomCats.map((cat) =>
+		fetch(`/api/spotify/browse/categories/${cat.id}/playlists?limit=6`)
+	);
+
+	const [newReleasesRes, featuredPlaylistsRes, myPlaylistsRes, ...randomCatsRes] =
+		await Promise.all([newReleases, featuredPlaylists, myPlaylists, ...randomCatPromises]);
+
+	return {
+		newReleases: newReleasesRes.ok
+			? (newReleasesRes.json() as Promise<SpotifyApi.ListOfNewReleasesResponse>)
+			: undefined,
+		featuredPlaylists: featuredPlaylistsRes.ok
+			? (featuredPlaylistsRes.json() as Promise<SpotifyApi.ListOfFeaturedPlaylistsResponse>)
+			: undefined,
+		myPlaylists: myPlaylistsRes.ok
+			? (myPlaylistsRes.json() as Promise<SpotifyApi.ListOfUsersPlaylistsResponse>)
+			: undefined,
+		homeCategories: randomCats,
+		categoriesPlaylists: Promise.all(
+			randomCatsRes.map((res) =>
+				res.ok ? (res.json() as Promise<SpotifyApi.CategoryPlaylistsResponse>) : undefined
+			)
+		)
+	};
+};
