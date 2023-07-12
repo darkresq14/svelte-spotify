051dad390dbd8b4acc396d6263328614474ca5f1
Razvan Bielz <razvan.bielz@gmail.com>
2023-07-12 20:11:39 +0300

    Added error handling

--- src/lib/components/LogoutButton.svelte
+++ src/lib/components/LogoutButton.svelte
@@ -1,14 +1,18 @@
-<script lang='ts'>
-	import { invalidateAll } from "$app/navigation";
+<script lang="ts">
+	import { invalidateAll } from '$app/navigation';
+	import { Button } from '$components';
 
-  const logoutHandler = async () => {
-    const response = await fetch('api/auth/logout', { method: 'POST', headers: { 'accept': 'application/json' } });
-    if (response.ok) {
-      invalidateAll()
-    }
-  }
+	const logoutHandler = async () => {
+		const response = await fetch('api/auth/logout', {
+			method: 'POST',
+			headers: { accept: 'application/json' }
+		});
+		if (response.ok) {
+			invalidateAll();
+		}
+	};
 </script>
 
-<form method="post" action="/api/auth/logout" on:submit|preventDefault = {logoutHandler}>
-    <button type="submit">Logout</button>
+<form method="post" action="/api/auth/logout" on:submit|preventDefault={logoutHandler}>
+	<Button element="button" type="submit">Logout</Button>
 </form>
